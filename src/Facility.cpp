7b7d1f208294e599cbfcbac6fc3fe96fb05d6331
Ganot <alonganot@gmail.com>
2024-11-26 19:30:04 +0200

    kaki

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -25,3 +25,34 @@ int &FacilityType::getEconomyScore() const {
 FacilityCategory &FacilityType::getCategory() const {
     return category;
 }
+
+Facility::Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score) {}
+Facility::Facility(const FacilityType &type, const string &settlementName) {}
+
+const string &Facility::getSettlementName() const {
+    return settlementName;
+}
+
+const int &Facility::getTimeLeft() const {
+    return timeLeft;
+}
+
+FacilityStatus &Facility::step() {
+    if(timeLeft > 0) {
+        timeLeft--;
+    } else {
+        status = FacilityStatus::OPERATIONAL;
+    }
+}
+
+void &Facility::setStatus(FacilityStatus new_status) {
+    status = new_status;
+}
+
+const &Facility::getStatus() const {
+    return status;
+}
+
+const string &Facility::toString() const {
+    return 
+}
