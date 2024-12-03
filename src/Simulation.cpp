30b6637f36451286b9086e3dd96449cc3895e6d3
moshearz <160511903+moshearz@users.noreply.github.com>
2024-12-03 13:40:51 +0200

    Simulation.cpp changes

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -7,71 +7,62 @@
 #include <iostream>
 #include <algorithm>
 
-using namespace std
+using namespace std;
+// VERIFY RULE OF 5 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-// ===CONSTRUCTOR===
-
+// ===============================================CONSTRUCTOR=================================================
 Simulation::Simulation(const string &configFilePath) : isRunning(false), planCounter(0) {
-
     std::ifstream configFile(configFilePath);
 
     if(!configFile.open()){ throw std::runtime_error("Unable to open configuration file");}
 
     string line;
-    while (std::getline(configFile, line)) {
+    while (std::getline(configFile, line)){
 
         if (line.empty() || line[0] == '#') continue;
 
-        std::istringstream streammm(line);  
+        std::istringstream streamm(line);  
         string req_data;
-        streammm >> req_data;  
+        streamm >> req_data;  
 
         if (req_data == "settlement"){
 
             string name;
             int typeInt;
-            stream >> name >> typeInt;
+            streamm >> name >> typeInt;
 
-            AddSettlement::AddSettlement(name, static_cast<SettlementType>(typeInt));
+            AddSettlement(name, static_cast<SettlementType>(typeInt)).act(*this);
         }
-
-        if (req_data == "facility"){
-
+        else if (req_data == "facility"){
             string name;
             int category, price, life_quality_score, economy_score, environment_score;
-            stream >> name >> category >> price >> life_quality_score >> economy_score >> environment_score;
+            streamm >> name >> category >> price >> life_quality_score >> economy_score >> environment_score;
 
-            AddFacility::AddFacility(name, category, price, life_quality_score, economy_score, environment_score);
-
+            AddFacility::AddFacility(name, static_cast<FacilityCategory>(category), price, life_quality_score, economy_score, environment_score);
         }
-
-        if (req_data == "plan"){
-
-            string nameOfPlan, selectionPolicyForPlan;
-            stream >> name >> selectionPolicy;
+        else if (req_data == "plan"){
+            string nameOfsettlement, nameOfselectionPoliy;
+            streamm >> name >> selectionPolicy;
             
             SelectionPolicy* spForPlanCfile = Plan::createSelectionPolicy(selectionPolicy);
 
-            AddPlan::AddPlan(nameOfPlanm, selectionPolicyForPlan);
-        
-        }
+            AddPlan(nameOfPlanm, selectionPolicyForPlan).act(*this);
+            //maybe we need toclean up dynamically allocated memory ---> delete selectionPolicy ??????? Rule of 5 ????? 
+        }            
+    }
 
-        configFile.close();
-            
-        }
-
+    configFile.close();
 }
 
-
+// ===============================================METHODS IMPLEMANTIONS=================================================
 void Simulation::start(){
     isRunning = true;
-    std::cout << "The simulation has started. " << std::end1;
+    std::cout << "The simulation has started. " << std::endl;
     open();
 }
 
 void Simulation::addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy) {
-    plans.emplace_back(planCounter++, settlement, selectionPolicy, facilitiesOptions); //maybe needs to add &
+    plans.emplace_back(planCounter++, settlement, selectionPolicy, facilitiesOptions); 
 }
 
 void Simulation::addAction(BaseAction *action) {
@@ -79,24 +70,24 @@ void Simulation::addAction(BaseAction *action) {
 }
 
 bool Simulation::addSettlement(Settlement *settlement) {
-    if (isSettlementExists(settlement -> getName())) return {false;}
+    if (isSettlementExists(settlement -> getName())) return false;
     settlements.push_back(settlement);
     return true;
 }
 
 bool Simulation::addFacility(FacilityType Facility) {
     vector<FacilityType>::itearator ft_itr;
-    for (ft_itr = facilitiesOptions -> begin(); ft_itr != facilitiesOptions -> end(); ft_itr++) {
-        if (ft_itr -> getName() == Facility.getName()) {return false;}}
-
+    for (ft_itr = facilitiesOptions.begin(); ft_itr != facilitiesOptions.end(); ft_itr++) {
+        if (ft_itr->getName() == Facility.getName()) return false;
+    }
     facilitiesOptions.push_back(Facility);
     return true;
 }
 
 bool Simulation::isSettlementExists(const string &settlementName) {
-    vector<Settlement*>::iterator iter; //creation of itearator type of <Settlement*>
+    vector<Settlement*>::iterator iter; 
     for (iter = settlements.begin(); iter != settlements.end(); iter ++){
-        if(iter-> getName() == settlementName)
+        if((*iter)->getName() == settlementName)
             return true;
     }
     return false;
@@ -110,14 +101,15 @@ Settlement& Simulation::getSettlement(const string &settlementName) {
 Plan& Simulation::getPlan(const int planID){
     vector<Plan>::itearator iter;
     for(iter = plans.begin(); iter != plans.end(); iter++){
-        if(iter-> getPlanID() == planID)
+        if(iter->getPlanID() == planID)
             return *iter;
     }
 }
 
 void Simulation::step() {
-    vector<Plan>::iterator itr;
-    for (itr = plans.begin(); itr != plans.end();itr++) {itr -> Plan::step();}
+    vector<Plan>::iterator iter;
+    for(iter=plans.begin(); iter != plans.end(); iter++)
+        iter->Plan::step();
 }
 
 void Simulation::close() {
@@ -131,104 +123,75 @@ void Simulation::close() {
     
     for (vector<Settlement*>::iterator s_itr = settlements.begin(); s_itr != settlements.end(); s_itr++) {
         delete *s_itr;}
+
+    std::cout << "Simulation has been closed. Please come back :)" << std::endl;
 }
 
 void Simulation::open(){
-
-    string input;
-
     while(isRunning){
+        std::cout << "Please enter an action to execute" << std::endl;
 
-        std::cout << "Please enter an action to execute" << end1;
         std::string actionLine;
-        std::getline(std::cin, std::actionLine);
+        std::getline(std::cin, actionLine);
 
         if (actionLine.empty()) continue;
 
         std::istringstream inputStreamm(actionLine);
-        std::sring command;
+        std::string command;
         inputStreamm >> command;
 
-        if (inputStreamm == "step"){
-
+        if (command == "step"){
             int numOfSteps;
             inputStreamm >> numOfSteps;
-            
             //if(inputStreamm.fail() || numOfSteps <= 0). "Thia action never results an error. we can assume numOfStep is always a number"
-
-            SimulateStep::SimulateStep(3).act(*this); // this is pointer to the current object(simulation)
-
-            // this is an lvalue, but we do not need this action afterwards so we will use the upperline.
-            //SimulateStep stepFromAction = SimulateStep(3);
-           // stepFromAction.act(this);
+            SimulateStep(numOfSteps).act(*this); // this is pointer to the current object(simulation)
         }
-
-        else if (inputStreamm == "plan"){
-
-            std::sring settlement_name, selection_policy;
+        else if (command == "plan"){
+            std::string settlement_name, selection_policy;
             inputStreamm >> settlement_name >> selection_policy;
 
-            AddPlan::AddPlan(settlement_name, selection_policy).act(*this);
+            AddPlan(settlement_name, selection_policy).act(*this);
         }
-
-        else if (inputStreamm == "settlement"){
-
+        else if (command == "settlement"){
             std::string settlement_name, settlement_type;
             inputStreamm >> settlement_name >> settlement_type;
 
-            AddSettlement::AddSettlement(settlement_name, settlement_type).act(*this);
+            AddSettlement(settlement_name, settlement_type).act(*this);
         }
+        else if (command == "facility"){
 
-        else if (inputStreamm == "facillity"){
-
-            std::string facillity_name;
+            std::string facility_name;
             int category, price, lifeq_impact, eco_impact, env_impact;
-            inputStreamm >> facillity_name >> category >> price >> lifeq_impact >> eco_impact >> env_impact;
+            inputStreamm >> facility_name >> category >> price >> lifeq_impact >> eco_impact >> env_impact;
 
-            if (category == 0){
-                FacilityCategory category_convertFromInt = LIFE_QUALITY;
-            }
-            else if (category == 1){
-                FacilityCategory category_convertFromInt = ECONOMY;
-            
-            }
-            else (category == 2){
-                FacilityCategory category_convertFromInt = ENVIRONMENT;
-            }
 
-            AddFacility::AddFacility(facillity_name, category_convertFromInt, price, lifeq_impact,eco_impact,eco_impact, env_impact).act(*this);
+            FacilityCategory category_enum = static_cast<FacilityCategory>(category);
+         
+            AddFacility(facility_name, category_enum, price, lifeq_impact,eco_impact,eco_impact, env_impact).act(*this);
 
         }
-
-        else if (inputStreamm == "planStatus"){
-
+        else if (command == "planStatus"){
             int plan_id;
             inputStreamm >> plan_id;
 
-            PrintPlanStatus::PrintPlanStatus(plan_id).act(*this);
+            PrintPlanStatus(plan_id).act(*this);
         }
-
-        else if (inputStreamm == "changePolicy"){
-
+        else if (command == "changePolicy"){
             int plan_id;
             std::string selection_policy;
             inputStreamm >> plan_id >> selection_policy;
 
-            ChangePlanPolicy::ChangePlanPolicy(plan_id, selection_policy).act(*this);
+            ChangePlanPolicy(plan_id, selection_policy).act(*this);
         }
+        else if (command == "log") {PrintActionsLog().act(*this);}
+        
+        else if (command == "backup") {BackupSimulation().act(*this);}
 
-        else if (inputStreamm == "log"){
+        else if(command == "restore") {RestoreSimulation::RestoreSimulation().act(*this);}
 
-            PrintActionsLog::PrintActionsLog().act(*this);
-        }
+        else if (command == "close") {close();}
 
-        else if (inputStreamm == "backup") BackupSimulation::BackupSimulation().act(*this);
-
-        else if(inputStreamm == "restore") RestoreSimulation::RestoreSimulation().act(*this);
-
-        else if (inputStreamm == "close") Simulation::close();
-
-        else cout << "Invalid method. Please try again."
+        else {std::cout << "Invalid method. Please try again." << std::endl;}
 
     }
 
@@ -236,8 +199,9 @@ void Simulation::open(){
 
 bool Simulation::isPlanExists(Const int& planId) const {
     vector<Plan>::itearator p_iter;
-    for (p_iter = plans.begin(); p_iter != plans.end(); p_iter++) {
-        if (p_iter -> getPlanID() == planId) {return true;}
+    for (p_iter=plans.begin(); p_iter!=plans.end(); p_iter++) {
+        if (p_iter->getPlanID() == planId) 
+            return true;
     }
     return false;
 }
